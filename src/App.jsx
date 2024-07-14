1e8596699ce672a69a9692855a0b783bdbf7fb7b
Mskerz <mskz_wichasin@kkumail.com>
2024-07-14 22:35:40 +0700

    build mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-
-import './App.css'
+import React from 'react';
+import { Container, Typography, AppBar, Toolbar, Button } from '@mui/material';
 
 function App() {
-
   return (
-    <>
-        <div> Default MyPortfolio </div>
-    </>
-  )
+    <Container>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Welcome to My Portfolio
+      </Typography>
+      <Typography variant="body1">
+        This is where you can put an introduction about yourself.
+      </Typography>
+      <Button variant="contained" color="primary">
+        Contact Me
+      </Button>
+    </Container>
+  );
 }
 
-export default App
+export default App;
